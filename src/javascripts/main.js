1b200cfb4bd8495080cdd5e3cc9aa2bab7480de1
Andrew Tiller <tiller.andrew@gmail.com>
2019-04-20 11:14:37 -0500

    printing locations to dom

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,11 @@
 import movies from './components/movies/movies';
+import locations from './components/locations/locations';
 
 import '../styles/main.scss';
 
 const init = () => {
   movies.initializeMovies();
+  locations.initializeLocations();
 };
 
 init();
